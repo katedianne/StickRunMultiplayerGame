cf3c9b0d235b467eae3a2f188dc9c630d257923a
Jopolo-15 <jcamcam15@gmail.com>
2020-03-10 19:30:50 +0800

    BG, OBS, buttons

--- StickRunMultiplayerGame/Form1.Designer.vb
+++ StickRunMultiplayerGame/Form1.Designer.vb
@@ -23,15 +23,40 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Player = New System.Windows.Forms.PictureBox()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.Obs1 = New System.Windows.Forms.PictureBox()
         Me.Obs2 = New System.Windows.Forms.PictureBox()
         Me.Obs3 = New System.Windows.Forms.PictureBox()
+        Me.bg1 = New System.Windows.Forms.PictureBox()
+        Me.bg2 = New System.Windows.Forms.PictureBox()
+        Me.bg3 = New System.Windows.Forms.PictureBox()
+        Me.bg4 = New System.Windows.Forms.PictureBox()
+        Me.btnjump = New System.Windows.Forms.PictureBox()
+        Me.btnslide = New System.Windows.Forms.PictureBox()
+        Me.ob1 = New System.Windows.Forms.PictureBox()
+        Me.ob2 = New System.Windows.Forms.PictureBox()
+        Me.ob3 = New System.Windows.Forms.PictureBox()
+        Me.ob4 = New System.Windows.Forms.PictureBox()
+        Me.ob5 = New System.Windows.Forms.PictureBox()
+        Me.ob6 = New System.Windows.Forms.PictureBox()
         CType(Me.Player, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Obs1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Obs2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Obs3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.bg1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.bg2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.bg3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.bg4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btnjump, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btnslide, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ob1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ob2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ob3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ob4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ob5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ob6, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Player
@@ -73,11 +98,131 @@ Partial Class Form1
         Me.Obs3.TabIndex = 0
         Me.Obs3.TabStop = False
         '
+        'bg1
+        '
+        Me.bg1.BackgroundImage = CType(resources.GetObject("bg1.BackgroundImage"), System.Drawing.Image)
+        Me.bg1.Location = New System.Drawing.Point(12, 207)
+        Me.bg1.Name = "bg1"
+        Me.bg1.Size = New System.Drawing.Size(146, 149)
+        Me.bg1.TabIndex = 2
+        Me.bg1.TabStop = False
+        '
+        'bg2
+        '
+        Me.bg2.BackgroundImage = CType(resources.GetObject("bg2.BackgroundImage"), System.Drawing.Image)
+        Me.bg2.Location = New System.Drawing.Point(12, 0)
+        Me.bg2.Name = "bg2"
+        Me.bg2.Size = New System.Drawing.Size(208, 197)
+        Me.bg2.TabIndex = 3
+        Me.bg2.TabStop = False
+        '
+        'bg3
+        '
+        Me.bg3.BackgroundImage = CType(resources.GetObject("bg3.BackgroundImage"), System.Drawing.Image)
+        Me.bg3.Location = New System.Drawing.Point(399, 0)
+        Me.bg3.Name = "bg3"
+        Me.bg3.Size = New System.Drawing.Size(174, 112)
+        Me.bg3.TabIndex = 4
+        Me.bg3.TabStop = False
+        '
+        'bg4
+        '
+        Me.bg4.BackgroundImage = CType(resources.GetObject("bg4.BackgroundImage"), System.Drawing.Image)
+        Me.bg4.Location = New System.Drawing.Point(226, 0)
+        Me.bg4.Name = "bg4"
+        Me.bg4.Size = New System.Drawing.Size(144, 124)
+        Me.bg4.TabIndex = 5
+        Me.bg4.TabStop = False
+        '
+        'btnjump
+        '
+        Me.btnjump.BackgroundImage = CType(resources.GetObject("btnjump.BackgroundImage"), System.Drawing.Image)
+        Me.btnjump.Location = New System.Drawing.Point(607, 0)
+        Me.btnjump.Name = "btnjump"
+        Me.btnjump.Size = New System.Drawing.Size(166, 133)
+        Me.btnjump.TabIndex = 6
+        Me.btnjump.TabStop = False
+        '
+        'btnslide
+        '
+        Me.btnslide.BackgroundImage = CType(resources.GetObject("btnslide.BackgroundImage"), System.Drawing.Image)
+        Me.btnslide.Location = New System.Drawing.Point(723, 166)
+        Me.btnslide.Name = "btnslide"
+        Me.btnslide.Size = New System.Drawing.Size(171, 125)
+        Me.btnslide.TabIndex = 7
+        Me.btnslide.TabStop = False
+        '
+        'ob1
+        '
+        Me.ob1.BackgroundImage = CType(resources.GetObject("ob1.BackgroundImage"), System.Drawing.Image)
+        Me.ob1.Location = New System.Drawing.Point(187, 130)
+        Me.ob1.Name = "ob1"
+        Me.ob1.Size = New System.Drawing.Size(326, 292)
+        Me.ob1.TabIndex = 8
+        Me.ob1.TabStop = False
+        '
+        'ob2
+        '
+        Me.ob2.BackgroundImage = CType(resources.GetObject("ob2.BackgroundImage"), System.Drawing.Image)
+        Me.ob2.Location = New System.Drawing.Point(551, 221)
+        Me.ob2.Name = "ob2"
+        Me.ob2.Size = New System.Drawing.Size(87, 93)
+        Me.ob2.TabIndex = 9
+        Me.ob2.TabStop = False
+        '
+        'ob3
+        '
+        Me.ob3.BackgroundImage = CType(resources.GetObject("ob3.BackgroundImage"), System.Drawing.Image)
+        Me.ob3.Location = New System.Drawing.Point(656, 297)
+        Me.ob3.Name = "ob3"
+        Me.ob3.Size = New System.Drawing.Size(79, 101)
+        Me.ob3.TabIndex = 10
+        Me.ob3.TabStop = False
+        '
+        'ob4
+        '
+        Me.ob4.BackgroundImage = CType(resources.GetObject("ob4.BackgroundImage"), System.Drawing.Image)
+        Me.ob4.Location = New System.Drawing.Point(775, 279)
+        Me.ob4.Name = "ob4"
+        Me.ob4.Size = New System.Drawing.Size(80, 89)
+        Me.ob4.TabIndex = 11
+        Me.ob4.TabStop = False
+        '
+        'ob5
+        '
+        Me.ob5.BackgroundImage = CType(resources.GetObject("ob5.BackgroundImage"), System.Drawing.Image)
+        Me.ob5.Location = New System.Drawing.Point(539, 340)
+        Me.ob5.Name = "ob5"
+        Me.ob5.Size = New System.Drawing.Size(75, 82)
+        Me.ob5.TabIndex = 12
+        Me.ob5.TabStop = False
+        '
+        'ob6
+        '
+        Me.ob6.BackgroundImage = CType(resources.GetObject("ob6.BackgroundImage"), System.Drawing.Image)
+        Me.ob6.Location = New System.Drawing.Point(619, 17)
+        Me.ob6.Name = "ob6"
+        Me.ob6.Size = New System.Drawing.Size(271, 198)
+        Me.ob6.TabIndex = 13
+        Me.ob6.TabStop = False
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(906, 528)
+        Me.Controls.Add(Me.ob6)
+        Me.Controls.Add(Me.ob5)
+        Me.Controls.Add(Me.ob4)
+        Me.Controls.Add(Me.ob3)
+        Me.Controls.Add(Me.ob2)
+        Me.Controls.Add(Me.ob1)
+        Me.Controls.Add(Me.btnslide)
+        Me.Controls.Add(Me.btnjump)
+        Me.Controls.Add(Me.bg4)
+        Me.Controls.Add(Me.bg3)
+        Me.Controls.Add(Me.bg2)
+        Me.Controls.Add(Me.bg1)
         Me.Controls.Add(Me.Obs3)
         Me.Controls.Add(Me.Obs2)
         Me.Controls.Add(Me.Obs1)
@@ -88,6 +233,18 @@ Partial Class Form1
         CType(Me.Obs1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.Obs2, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.Obs3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.bg1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.bg2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.bg3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.bg4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btnjump, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btnslide, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ob1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ob2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ob3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ob4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ob5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ob6, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -97,4 +254,16 @@ Partial Class Form1
     Friend WithEvents Obs1 As PictureBox
     Friend WithEvents Obs2 As PictureBox
     Friend WithEvents Obs3 As PictureBox
+    Friend WithEvents bg1 As PictureBox
+    Friend WithEvents bg2 As PictureBox
+    Friend WithEvents bg3 As PictureBox
+    Friend WithEvents bg4 As PictureBox
+    Friend WithEvents btnjump As PictureBox
+    Friend WithEvents btnslide As PictureBox
+    Friend WithEvents ob1 As PictureBox
+    Friend WithEvents ob2 As PictureBox
+    Friend WithEvents ob3 As PictureBox
+    Friend WithEvents ob4 As PictureBox
+    Friend WithEvents ob5 As PictureBox
+    Friend WithEvents ob6 As PictureBox
 End Class
