dcf162eeba815c48a4237c1ef6e0ff18c0e29441
ersonjn <ersonjn@gmail.com>
2020-03-11 18:37:13 +0800

    added main screen form
    added main screen form with buttons

---
+++ StickRunMultiplayerGame/Form2.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.PlayButton = New System.Windows.Forms.Button()
+        Me.MultiplayerButton = New System.Windows.Forms.Button()
+        Me.OptionButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(370, 468)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(26, 30)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(341, 101)
+        Me.Panel1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(146, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.ExitButton)
+        Me.Panel2.Controls.Add(Me.OptionButton)
+        Me.Panel2.Controls.Add(Me.MultiplayerButton)
+        Me.Panel2.Controls.Add(Me.PlayButton)
+        Me.Panel2.Location = New System.Drawing.Point(120, 175)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(141, 177)
+        Me.Panel2.TabIndex = 2
+        '
+        'PlayButton
+        '
+        Me.PlayButton.Location = New System.Drawing.Point(3, 3)
+        Me.PlayButton.Name = "PlayButton"
+        Me.PlayButton.Size = New System.Drawing.Size(135, 38)
+        Me.PlayButton.TabIndex = 0
+        Me.PlayButton.Text = "Play"
+        Me.PlayButton.UseVisualStyleBackColor = True
+        '
+        'MultiplayerButton
+        '
+        Me.MultiplayerButton.Location = New System.Drawing.Point(3, 47)
+        Me.MultiplayerButton.Name = "MultiplayerButton"
+        Me.MultiplayerButton.Size = New System.Drawing.Size(135, 38)
+        Me.MultiplayerButton.TabIndex = 1
+        Me.MultiplayerButton.Text = "Multiplayer"
+        Me.MultiplayerButton.UseVisualStyleBackColor = True
+        '
+        'OptionButton
+        '
+        Me.OptionButton.Location = New System.Drawing.Point(3, 91)
+        Me.OptionButton.Name = "OptionButton"
+        Me.OptionButton.Size = New System.Drawing.Size(135, 38)
+        Me.OptionButton.TabIndex = 2
+        Me.OptionButton.Text = "Option"
+        Me.OptionButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(3, 135)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(135, 38)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(394, 492)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents OptionButton As Button
+    Friend WithEvents MultiplayerButton As Button
+    Friend WithEvents PlayButton As Button
+End Class
